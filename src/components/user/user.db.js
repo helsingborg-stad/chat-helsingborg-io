14dbb1f210ccb0e2cd55c51a1d676ef665675748
Nikolas Ramstedt <nikolas.ramstedt@gmail.com>
2019-08-02 15:22:21 +0200

    Mock users

---
+++ src/components/user/user.db.js
@@ -0,0 +1,18 @@
+// const { client, extractQueryParts } = require('../../db/db.client');
+
+const mockUsers = [
+  {
+    id: 1,
+    fistName: 'Sean',
+    lastName: 'Banan',
+    clientId: false,
+  },
+  {
+    id: 2,
+    fistName: 'Mikael',
+    lastName: 'Persbrandt',
+    clientId: false,
+  },
+];
+
+module.exports = mockUsers;
